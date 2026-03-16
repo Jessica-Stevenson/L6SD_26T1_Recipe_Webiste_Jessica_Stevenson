eb097dd89c4ba581478280444b694117cfa75a93
Jessica <Jessica@DESKTOP-U34H195>
2026-03-17 09:33:49 +1300

    fixed spelling mistake

---
+++ recipesite/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('health_diet/', views.health_diet_view, name='health_diet'),
+    path('holidays/', views.holidays_view, name='holidays'),
+    path('signup/', views.signup_view, name='signup'),
+    path('breakfast/', views.breakfast_view, name='breakfast'),
+    path('lunch/', views.lunch_view, name='lunch'),
+    path('dinner/', views.dinner_view, name='dinner'),
+    path('recipe/<int:pk>/', views.recipe_detail_view, name='recipe_detail'),
+    path('profile/', views.profile_view, name='profile'),
+    path('recipe/create/', views.create_recipe_view, name='create_recipe'),
+    path('recipe/<int:pk>/delete/', views.delete_recipe_view, name='delete_recipe'),
+]
---
+++ recipesite/recipe/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from .models import Recipe
+from .models import Profile
+from .forms import RecipeForm
+from django.http import HttpResponseForbidden
+
+@login_required
+def delete_recipe_view(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+
+    if recipe.user != request.user:
+        return HttpResponseForbidden("You cannot delete this recipe.")
+
+    if request.method == "POST":
+        recipe.delete()
+        return redirect('home')
+
+    return render(request, 'food/delete_recipe.html', {'recipe': recipe})
+
+@login_required
+def create_recipe_view(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
+            return redirect('home') 
+    else:
+        form = RecipeForm()
+
+    return render(request, 'food/create_recipe.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'registration/profile.html', {'profile': profile})
+
+def home_view(request):
+    recipes = Recipe.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'recipes': recipes})
+
+def health_diet_view(request):
+    recipes = Recipe.objects.filter(category='health').order_by('-created_at')
+    return render(request, 'food/health_diet.html', {'recipes': recipes})
+
+def holidays_view(request):
+    recipes = Recipe.objects.filter(category='holiday').order_by('-created_at')
+    return render(request, 'food/holidays.html', {'recipes': recipes})
+
+def breakfast_view(request):
+    recipes = Recipe.objects.filter(category='breakfast').order_by('-created_at')
+    return render(request, 'food/daily/breakfast.html', {'recipes': recipes})
+
+def lunch_view(request):
+    recipes = Recipe.objects.filter(category='lunch').order_by('-created_at')
+    return render(request, 'food/daily/lunch.html', {'recipes': recipes})
+
+def dinner_view(request):
+    recipes = Recipe.objects.filter(category='dinner').order_by('-created_at')
+    return render(request, 'food/daily/dinner.html', {'recipes': recipes})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+
+def recipe_detail_view(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'food/recipe_detail.html', {'recipe': recipe})
